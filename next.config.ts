24553d28a56dc9e05e83ee3a35f37693bb5c0ab7
Mehjung <isla127@googlemail.com>
2024-12-29 18:24:27 +0100

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/PWA_Time_Tools',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
