aff919d3101f53e2a438717bf8da6e19e45ed1e0
Mehjung <isla127@googlemail.com>
2024-12-29 18:29:45 +0100

    Update deployment configuration for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
   basePath: '/PWA_Time_Tools',
   images: {
     unoptimized: true,
   },
-};
+  assetPrefix: '/PWA_Time_Tools/',
+}
 
 export default nextConfig;
